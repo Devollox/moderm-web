b65507636622f93f31ac2e05dcfc80b1010c36c0
Devollox <122895078+Devollox@users.noreply.github.com>
2024-05-24 20:41:25 +0300

    Update computer.js

--- pages/computer.js
+++ pages/computer.js
@@ -4,11 +4,9 @@ import getPosts from '@lib/get-posts'
 export const getStaticProps = () => { const posts = getPosts(); return { props: { posts } }}
 const Computer = () => {
   return (
-    <Page
-      title="About"
-      description="My short biography."
-    >
-    </Page>
+    <Error status={400}>
+
+    </Error>
   )
 }
 
