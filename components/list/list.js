5c1f7382864f18131502ba3a7304ca578b08dafc
Devollox <anisin.97@bk.ru>
2024-05-26 13:23:49 +0300

    update page

--- components/list/list.js
+++ components/list/list.js
@@ -9,9 +9,9 @@ const ListIndex = ({
   return (
     <ul style={{ margin: '0' }}>
       <li className={styles.list}>
-        <a className={styles.ul} href={url} target='_blank'>{title}</a><span
-        style={{ color: 'var(--colors-secondary)' }}> - </span><span
-        style={{ color: 'var(--colors-secondary)' }}>{description}</span>
+        <a className={styles.ul} href={url}>{title}</a><span
+        style={{ color: 'var(--colors-secondary)'}}> - </span><span
+        style={{ color: 'var(--colors-secondary)'}}>{description}</span>
       </li>
     </ul>
   )
