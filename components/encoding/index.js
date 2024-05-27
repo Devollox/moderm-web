1b4f3defdbe45e0428e82c7ff401a0c4a0d0c9cb
Devollox <anisin.97@bk.ru>
2024-05-27 21:58:41 +0300

    fix

--- components/encoding/index.js
+++ components/encoding/index.js
@@ -1,42 +1,44 @@
 import { useLayoutEffect } from 'react'
 
 const Encoding =  (intervalId, id) => {
-  useLayoutEffect(() => {
-    const targetText = document.getElementById(`${id}`);
-    if (targetText !== null) {
-      const targetString = targetText.textContent;
-      const alphabet = " abcdefghijklmnopqrstuvwxyz";
-      const interval = intervalId;
+  setTimeout(() => {
+    useLayoutEffect(() => {
+      const targetText = document.getElementById(`${id}`);
+      if (targetText !== null) {
+        const targetString = targetText.textContent;
+        const alphabet = " abcdefghijklmnopqrstuvwxyz";
+        const interval = intervalId;
 
-      function getRandomChar() {
-        return alphabet[Math.floor(Math.random() * alphabet.length)];
-      }
+        function getRandomChar() {
+          return alphabet[Math.floor(Math.random() * alphabet.length)];
+        }
 
-      function animateText() {
-        let currentString = Array(targetString.length).fill("");
-        let index = 0;
+        function animateText() {
+          let currentString = Array(targetString.length).fill("");
+          let index = 0;
 
-        const intervalId = setInterval(() => {
-          for (let i = 0; i < currentString.length; i++) {
-            if (i < index) {
-              currentString[i] = targetString[i];
-            } else {
-              currentString[i] = getRandomChar();
+          const intervalId = setInterval(() => {
+            for (let i = 0; i < currentString.length; i++) {
+              if (i < index) {
+                currentString[i] = targetString[i];
+              } else {
+                currentString[i] = getRandomChar();
+              }
             }
-          }
 
-          targetText.textContent = currentString.join("");
+            targetText.textContent = currentString.join("");
 
-          if (index >= targetString.length) {
-            clearInterval(intervalId);
-          } else {
-            index++;
-          }
-        }, interval);
+            if (index >= targetString.length) {
+              clearInterval(intervalId);
+            } else {
+              index++;
+            }
+          }, interval);
+        }
+
+        animateText()
       }
-
-      animateText()
-    }
+    },10)
   })
 }
 
